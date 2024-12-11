e8a6f1efc14964e16d95c0a658618238dfa44d3a
finn <burroughszeb@me.com>
2024-12-11 15:25:55 -0600

    horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\ContentParsingProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
 ];
